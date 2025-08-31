2dead9d84c3513be9c26da0e6357af2549734dcf
fatima <fatima.amani65@gmail.com>
2025-08-31 20:05:09 +0530

    add query for create table (3NF) and insert data

---
+++ schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Customer (
+	CustomerID SERIAL PRIMARY KEY,
+	CustomerName VARCHAR(20) NOT NULL 
+);
+
+CREATE TABLE Meter (
+	MeterID SERIAL PRIMARY KEY,
+	CustomerID INT NOT NULL REFERENCES Customer(CustomerID)
+);
+
+
+CREATE TABLE MeterReading (
+    MeterReadingID SERIAL PRIMARY KEY,
+    MeterID INT NOT NULL REFERENCES Meter(MeterID),
+    ReadingDate DATE NOT NULL,
+    PreviousReadingID INT REFERENCES MeterReading(MeterReadingID),
+    CurrentReading DECIMAL(10,2) NOT NULL,
+    ReadingType VARCHAR(20),
+    Status BIT,
+    Remarks VARCHAR(255) NULL
+);
+
+INSERT INTO CUSTOMER (CustomerName) VALUES 
+	('FATIMA'),
+	('AAMNA'),
+	('APEKSHA');
+
+INSERT INTO Meter (CustomerID) VALUES
+	('1'),
+	('2'),
+	('3');
+
+
+INSERT INTO MeterReading (MeterID, ReadingDate, PreviousReadingID, CurrentReading, ReadingType, Status, Remarks)
+VALUES
+(1, '2025-07-10', NULL, 100.00, 'Manual',  B'1', 'reading for July'),
+(2, '2025-07-11', NULL, 150.50, 'Automatic',  B'1', 'reading for July'),
+(3, '2025-07-12', NULL, 200.75, 'Estimated',  B'1', 'reading for July');
+
+
+INSERT INTO MeterReading (MeterID, ReadingDate, PreviousReadingID, CurrentReading, ReadingType, Status, Remarks)
+VALUES
+(1, '2025-08-10', 1, 120.00, 'Manual',  B'1', 'reading for August'),
+(2, '2025-08-11', 2, 170.25, 'Automatic',  B'1', 'reading for August'),
+(3, '2025-08-12', 3, 220.50, 'Estimated',  B'1', 'reading for August');
+
+
+
+
+
